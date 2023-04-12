0f1d251dc33b998085a46f0a5f698d4ce207d47d
Gabriel Vicente <gabrielpqdx@gmail.com>
2023-04-12 12:44:32 -0300

    Dispositivos slaves criados e rotina de reconhecimento do slave na função de callback do mestre

--- Firmware/IotSensorEscravo/IotSensorEscravo.ino
+++ Firmware/IotSensorEscravo/IotSensorEscravo.ino
@@ -1,32 +1,13 @@
-#include <Arduino.h>
+//#include <Arduino.h>
 #include <esp_now.h>
 #include <esp_wifi.h>
 #include <WiFi.h>
 #include <EEPROM.h>
 
 
-#define MAX_CHANNEL 11  // for North America // 13 in Europe
+#define MAX_CHANNEL 11
 
 uint8_t Broadcast[] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-
-typedef struct struct_message {
-  uint8_t msgType;
-  uint8_t Device_Id;
-  float temp;
-  float hum;
-  bool on_off;
-} struct_message;
-
-typedef struct struct_pairing {   
-    uint8_t msgType;
-    uint8_t Device_Id;
-    uint8_t macAddr[6];
-    uint8_t channel;
-} struct_pairing;
-
-struct_message DataSent;
-struct_message DataRecv;
-struct_pairing pairingData;
 
 enum PairingStatus {NOT_PAIRED, PAIR_REQUEST, PAIR_REQUESTED, PAIR_PAIRED,};
 PairingStatus pairingStatus = NOT_PAIRED;
@@ -34,31 +15,55 @@ PairingStatus pairingStatus = NOT_PAIRED;
 enum MessageType {PAIRING, DATA,};
 MessageType messageType;
 
-enum DeviceId {MESTRE, ESCRAVO,};
-DeviceId DeviceId = ESCRAVO;
+enum DeviceType {MESTRE, UMIDADE, TEMPERATURA, NIVEL};
+DeviceType DeviceType = MESTRE;
+
+typedef struct struct_message {
+  uint8_t msgType;
+  uint8_t Device_Type;
+  uint8_t Device_Id;
+  float temp;
+  float hum;
+  bool on_off;
+} struct_message;
+struct_message DataSent;
+struct_message DataRecv;
+
+typedef struct struct_pairing {   
+    uint8_t msgType;
+    uint8_t Device_Type;
+    uint8_t Device_Id;
+    uint8_t macAddr[6];
+    uint8_t channel;
+} struct_pairing;
+struct_pairing pairingData;
 
 #ifdef SAVE_CHANNEL
   int lastChannel;
 #endif  
-int channel = 1;
+uint8_t channel = 1;
  
 unsigned long currentMillis = millis();
 unsigned long previousMillis = 0;   // Stores last time temperature was published
-const long interval = 10000;        // Interval at which to publish sensor readings
-unsigned long start;                // used to measure Pairing time
-unsigned int readingId = 0;   
+const long interval = 3000;        // Interval at which to publish sensor readings
+bool led_state_ant = false; 
 
-
-void addPeer(const uint8_t * mac_addr, uint8_t chan){
+void addPeer(const uint8_t *mac_addr, uint8_t chan)
+{
   esp_now_peer_info_t peer;
   ESP_ERROR_CHECK(esp_wifi_set_channel(chan ,WIFI_SECOND_CHAN_NONE));
   esp_now_del_peer(mac_addr);
   memset(&peer, 0, sizeof(esp_now_peer_info_t));
   peer.channel = chan;
-  peer.encrypt = false;
-  memcpy(peer.peer_addr, mac_addr, sizeof(uint8_t[6]));
-  if (esp_now_add_peer(&peer) != ESP_OK){
-    return;
+  peer.encrypt = 0;
+  memcpy(peer.peer_addr, mac_addr, sizeof(uint8_t [6]));
+  esp_err_t addStatus = esp_now_add_peer(&peer);
+  if (addStatus == ESP_OK) {
+    Serial.println("Pareado");
+  }
+  else 
+  {
+    Serial.println("Falha ao parear");
   }
   memcpy(Broadcast, mac_addr, sizeof(uint8_t[6]));
 }
@@ -70,13 +75,13 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *Data, int len) {
   uint8_t type = Data[0];
   switch (type) 
   {
-    case DATA :      // we received data from server
+    case DATA:
       memcpy(&DataRecv, Data, sizeof(DataRecv));
       break;
 
     case PAIRING:    // we received pairing data from server
       memcpy(&pairingData, Data, sizeof(pairingData));
-      if (pairingData.Device_Id == MESTRE) 
+      if (pairingData.Device_Type == MESTRE) 
       { 
         addPeer(pairingData.macAddr, pairingData.channel); // add the server  to the peer list 
         #ifdef SAVE_CHANNEL
@@ -94,42 +99,33 @@ PairingStatus autoPairing(){
   switch(pairingStatus) 
   {
     case PAIR_REQUEST:
-    Serial.print("Pairing request on channel "  );
-    Serial.println(channel);
-
-    // set WiFi channel   
-    ESP_ERROR_CHECK(esp_wifi_set_channel(channel,  WIFI_SECOND_CHAN_NONE));
-    if (esp_now_init() != ESP_OK) {
-    }
-
-    // set callback routines
-    esp_now_register_send_cb(OnDataSent);
-    esp_now_register_recv_cb(OnDataRecv);
-  
-    // set pairing data to send to the server
-    pairingData.msgType = PAIRING;
-    pairingData.Device_Id = ESCRAVO;    
-    pairingData.channel = channel;
-
-    // add peer and send request
-    addPeer(Broadcast, channel);
-    esp_now_send(Broadcast, (uint8_t *) &pairingData, sizeof(pairingData));
-    previousMillis = millis();
-    pairingStatus = PAIR_REQUESTED;
+      // set WiFi channel   
+      ESP_ERROR_CHECK(esp_wifi_set_channel(channel,  WIFI_SECOND_CHAN_NONE));
+      if (esp_now_init() != ESP_OK) {
+      }
+      esp_now_register_send_cb(OnDataSent);
+      esp_now_register_recv_cb(OnDataRecv);
+      pairingData.msgType = PAIRING;
+      pairingData.Device_Type = TEMPERATURA;    
+      pairingData.channel = channel;
+      addPeer(Broadcast, channel);
+      esp_now_send(Broadcast, (uint8_t *) &pairingData, sizeof(pairingData));
+      previousMillis = millis();
+      pairingStatus = PAIR_REQUESTED;
     break;
 
     case PAIR_REQUESTED:
-    // time out to allow receiving response from server
-    currentMillis = millis();
-    if(currentMillis - previousMillis > 250) {
-      previousMillis = currentMillis;
-      // time out expired,  try next channel
-      channel ++;
-      if (channel > MAX_CHANNEL){
-         channel = 1;
-      }   
-      pairingStatus = PAIR_REQUEST;
-    }
+      // time out to allow receiving response from server
+      currentMillis = millis();
+      if(currentMillis - previousMillis > 250) {
+        previousMillis = currentMillis;
+        // time out expired,  try next channel
+        channel ++;
+        if (channel > MAX_CHANNEL){
+          channel = 1;
+        }   
+        pairingStatus = PAIR_REQUEST;
+      }
     break;
 
     case PAIR_PAIRED:
@@ -143,10 +139,8 @@ PairingStatus autoPairing(){
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(0, INPUT_PULLUP);
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  start = millis();
 
   #ifdef SAVE_CHANNEL 
     EEPROM.begin(10);
@@ -165,14 +159,14 @@ void loop()
     if (currentMillis - previousMillis >= interval) 
     {
       // Save the last time a new reading was published
+
+      if(led_state_ant == true) digitalWrite(LED_BUILTIN, false), led_state_ant = false;
+      else digitalWrite(LED_BUILTIN, true), led_state_ant = true;
       previousMillis = currentMillis;
       DataSent.msgType = DATA;
-      DataSent.Device_Id = ESCRAVO;
-      //DataSent.temp = 111.4;
-      DataSent.hum = 12.4;
-      DataSent.on_off = true;
-      esp_err_t result = esp_now_send(Broadcast, (uint8_t *) &DataSent, sizeof(DataSent));
+      DataSent.Device_Type = TEMPERATURA;
+      DataSent.temp = random(100);
+      esp_now_send(Broadcast, (uint8_t *) &DataSent, sizeof(DataSent));
     }
   }
-  if(digitalRead(0) == false) DataSent.temp += 10, delay(250);
 }
--- Firmware/Mestre/IotSensorMestre/IotSensorMestre.ino
+++ Firmware/Mestre/IotSensorMestre/IotSensorMestre.ino
@@ -1,151 +1,83 @@
 #include <esp_now.h>
 #include <WiFi.h>
-#include "ESPAsyncWebServer.h"
-#include "AsyncTCP.h"
+//#include "ESPAsyncWebServer.h"
+//#include "AsyncTCP.h"
 #include <ArduinoJson.h>
 
-// Replace with your network credentials (STATION)
 const char* ssid = "iPhone de Gabriel";
 const char* password = "12345678";
 
-esp_now_peer_info_t slave;
-int chan; 
+int channel; 
 
 int counter = 0;
 
+enum MessageType {PAIRING, DATA,};
+MessageType messageType;
+
+enum DeviceType {MESTRE, UMIDADE, TEMPERATURA, NIVEL};
+DeviceType DeviceType = MESTRE;
+
 typedef struct struct_message {
   uint8_t msgType;
+  uint8_t Device_Type;
   uint8_t Device_Id;
   float temp;
   float hum;
   bool on_off;
 } struct_message;
+struct_message DataSent;
+struct_message DataRecv;
 
 typedef struct struct_pairing {   
     uint8_t msgType;
+    uint8_t Device_Type;
     uint8_t Device_Id;
     uint8_t macAddr[6];
     uint8_t channel;
 } struct_pairing;
-
-struct_message DataSent;
-struct_message DataRecv;
 struct_pairing pairingData;
 
-enum MessageType {PAIRING, DATA,};
-MessageType messageType;
-
-enum DeviceId {MESTRE, ESCRAVO,};
-DeviceId DeviceId = MESTRE;
-
-AsyncWebServer server(80);
-AsyncEventSource events("/events");
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <title>ESP-NOW DASHBOARD</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <link rel="icon" href="data:,">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    p {  font-size: 1.2rem;}
-    body {  margin: 0;}
-    .topnav { overflow: hidden; background-color: #2f4468; color: white; font-size: 1.7rem; }
-    .content { padding: 20px; }
-    .card { background-color: white; box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5); }
-    .cards { max-width: 700px; margin: 0 auto; display: grid; grid-gap: 2rem; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); }
-    .reading { font-size: 2.8rem; }
-    .packet { color: #bebebe; }
-    .card.temperature { color: #fd7e14; }
-    .card.humidity { color: #1b78e2; }
-  </style>
-</head>
-<body>
-  <div class="topnav">
-    <h3>ESP-NOW DASHBOARD</h3>
-  </div>
-  <div class="content">
-    <div class="cards">
-      <div class="card temperature">
-        <h4><i class="fas fa-thermometer-half"></i> BOARD #1 - TEMPERATURE</h4><p><span class="reading"><span id="t1"></span> &deg;C</span></p><p class="packet">Reading ID: <span id="rt1"></span></p>
-      </div>
-      <div class="card humidity">
-        <h4><i class="fas fa-tint"></i> BOARD #1 - HUMIDITY</h4><p><span class="reading"><span id="h1"></span> &percnt;</span></p><p class="packet">Reading ID: <span id="rh1"></span></p>
-      </div>
-      <div class="card temperature">
-        <h4><i class="fas fa-thermometer-half"></i> BOARD #2 - TEMPERATURE</h4><p><span class="reading"><span id="t2"></span> &deg;C</span></p><p class="packet">Reading ID: <span id="rt2"></span></p>
-      </div>
-      <div class="card humidity">
-        <h4><i class="fas fa-tint"></i> BOARD #2 - HUMIDITY</h4><p><span class="reading"><span id="h2"></span> &percnt;</span></p><p class="packet">Reading ID: <span id="rh2"></span></p>
-      </div>
-    </div>
-  </div>
-<script>
-if (!!window.EventSource) {
- var source = new EventSource('/events');
- 
- source.addEventListener('open', function(e) {
-  console.log("Events Connected");
- }, false);
- source.addEventListener('error', function(e) {
-  if (e.target.readyState != EventSource.OPEN) {
-    console.log("Events Disconnected");
-  }
- }, false);
- 
- source.addEventListener('message', function(e) {
-  console.log("message", e.data);
- }, false);
- 
- source.addEventListener('new_readings', function(e) {
-  console.log("new_readings", e.data);
-  var obj = JSON.parse(e.data);
-  document.getElementById("t"+obj.id).innerHTML = obj.temperature.toFixed(2);
-  document.getElementById("h"+obj.id).innerHTML = obj.humidity.toFixed(2);
-  document.getElementById("rt"+obj.id).innerHTML = obj.readingId;
-  document.getElementById("rh"+obj.id).innerHTML = obj.readingId;
- }, false);
-}
-</script>
-</body>
-</html>)rawliteral";
-
-bool addPeer(const uint8_t *peer_addr) {      // add pairing
-  memset(&slave, 0, sizeof(slave));
-  const esp_now_peer_info_t *peer = &slave;
-  memcpy(slave.peer_addr, peer_addr, 6);
+typedef struct {
+  uint8_t device_addr[6];
   
-  slave.channel = chan; // pick a channel
-  slave.encrypt = 0; // no encryption
-  // check if the peer exists
-  bool exists = esp_now_is_peer_exist(slave.peer_addr);
-  if (exists) {
-    // Slave already paired.
-    Serial.println("Already Paired");
+} mac_addr_t;
+mac_addr_t device_list[20];
+
+bool addPeer(const uint8_t *peer_addr) 
+{
+  bool exists = esp_now_is_peer_exist(peer_addr);
+  if (exists) 
+  {
+    Serial.println("Dispositivo já pareado");
     return true;
   }
-  else {
-    esp_err_t addStatus = esp_now_add_peer(peer);
+  else
+  {
+    esp_now_peer_info_t peer;
+    memset(&peer, 0, sizeof(esp_now_peer_info_t));
+    peer.channel = channel;
+    peer.encrypt = 0;
+    memcpy(peer.peer_addr, peer_addr, 6);
+    esp_err_t addStatus = esp_now_add_peer(&peer);
     if (addStatus == ESP_OK) {
-      // Pair success
-      Serial.println("Pair success");
+      Serial.println("Pareado");
       return true;
     }
     else 
     {
-      Serial.println("Pair failed");
+      Serial.println("Falha ao parear");
       return false;
     }
   }
+
 } 
 
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) 
+{
 }
 
-void OnDataRecv(const uint8_t * mac_addr, const uint8_t *Data, int len) { 
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *Data, int len) 
+{ 
   StaticJsonDocument<1000> root;
   String payload;
   uint8_t type = Data[0];       // first message byte is the type of message 
@@ -154,26 +86,47 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *Data, int len) {
     case DATA :                           // the message is data type
       memcpy(&DataRecv, Data, sizeof(DataRecv));
       // create a JSON document with received data and send it by event to the web page
-      root["id"] = DataRecv.Device_Id;
+      /*root["id"] = DataRecv.Device_Id;
       root["temperature"] = DataRecv.temp;
       root["humidity"] = DataRecv.hum;
-      root["ON_OFF"] = DataRecv.on_off;;
+      root["ON_OFF"] = DataRecv.on_off;
       serializeJson(root, payload);
-      serializeJson(root, Serial);
-      events.send(payload.c_str(), "new_readings", millis());
+      serializeJson(root, Serial);*/
+      switch(DataRecv.Device_Type)
+      {
+        case UMIDADE:
+          Serial.println(DataRecv.Device_Id);
+          //Serial.println(mac_addr);
+          Serial.println(DataRecv.temp);
+          break;
+        case NIVEL:
+          break;
+        case TEMPERATURA:
+          Serial.println("-----------------------------");
+          Serial.print("Dispositivo: ");
+          Serial.println(DataRecv.Device_Id);
+          Serial.println("Tipo: Temperatura");
+          Serial.print("Temperatura Atual: ");
+          //Serial.println(mac_addr);
+          Serial.print(DataRecv.temp);
+          Serial.println("°C");
+          Serial.println("-----------------------------");
+          break;
+      }
+
       break;
     
     case PAIRING:                            // the message is a pairing request 
       memcpy(&pairingData, Data, sizeof(pairingData));
-      if (pairingData.Device_Id == ESCRAVO) 
+      if (pairingData.Device_Type != MESTRE) 
       {     // do not replay to server itself
         if (pairingData.msgType == PAIRING) 
         { 
-          pairingData.Device_Id = MESTRE;       // 0 is server
+          pairingData.Device_Type = MESTRE;
           // Server is in AP_STA mode: peers need to send data to server soft AP MAC address 
           WiFi.softAPmacAddress(pairingData.macAddr);   
-          pairingData.channel = chan;
-          esp_err_t result = esp_now_send(mac_addr, (uint8_t *) &pairingData, sizeof(pairingData));
+          pairingData.channel = channel;
+          esp_now_send(mac_addr, (uint8_t *) &pairingData, sizeof(pairingData));
           addPeer(mac_addr);
         }  
       }  
@@ -181,23 +134,10 @@ void OnDataRecv(const uint8_t * mac_addr, const uint8_t *Data, int len) {
   }
 }
 
-void initESP_NOW(){
-    // Init ESP-NOW
-    if (esp_now_init() != ESP_OK) {
-      esp_restart();
-      return;
-    }
-    esp_now_register_send_cb(OnDataSent);
-    esp_now_register_recv_cb(OnDataRecv);
-} 
 
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
-
-  Serial.println();
-  Serial.print("Server MAC Address:  ");
-  Serial.println(WiFi.macAddress());
 
   // Set the device as a Station and Soft Access Point simultaneously
   WiFi.mode(WIFI_AP_STA);
@@ -208,44 +148,17 @@ void setup() {
     Serial.println("Setting as a Wi-Fi Station..");
   }
 
-  Serial.print("Server SOFT AP MAC Address:  ");
-  Serial.println(WiFi.softAPmacAddress());
+  channel = WiFi.channel();
 
-  chan = WiFi.channel();
-  Serial.print("Station IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Wi-Fi Channel: ");
-  Serial.println(WiFi.channel());
-
-  initESP_NOW();
-  
-  // Start Web server
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
-  
-
-  // Events 
-  events.onConnect([](AsyncEventSourceClient *client){
-    if(client->lastId()){
-      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
-    }
-    // send event with message "hello!", id current millis
-    // and set reconnect delay to 1 second
-    client->send("hello!", NULL, millis(), 10000);
-  });
-  server.addHandler(&events);
-  
-  // start server
-  server.begin();
-
-}
-
-void loop() {
-  static unsigned long lastEventTime = millis();
-  static const unsigned long EVENT_INTERVAL_MS = 5000;
-  if ((millis() - lastEventTime) > EVENT_INTERVAL_MS) {
-    events.send("ping",NULL,millis());
-    lastEventTime = millis();
+  if (esp_now_init() != ESP_OK) 
+  {
+    esp_restart();
+    return;
   }
+  Serial.println("Iniciando");
+  esp_now_register_send_cb(OnDataSent);
+  esp_now_register_recv_cb(OnDataRecv);
+
 }
+
+void loop() {}
